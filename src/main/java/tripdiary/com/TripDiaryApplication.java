e961b4e652e37dcc437c87b7d1811376940e67d2
devdaeun <ekdms008@gmail.com>
2025-01-02 10:18:30 +0900

    Initial commit

---
+++ src/main/java/tripdiary/com/TripDiaryApplication.java
@@ -0,0 +1,11 @@
+package tripdiary.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripDiaryApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TripDiaryApplication.class, args);
+	}
+}

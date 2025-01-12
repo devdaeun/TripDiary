4fb5eb9cd23d7dc5b5d9154e79cf0a2f7e34115b
jeon <jejjb06@gmail.com>
2025-01-13 02:10:40 +0900

    우아.. 기억이 1도 나지 않는다...

---
+++ src/main/java/tripdiary/com/login/dto/loginDTO.java
@@ -0,0 +1,111 @@
+package tripdiary.com.login.dto;
+
+import org.springframework.stereotype.Component;
+
+import java.sql.Date;
+
+@Component("loginDTO")
+public class loginDTO {
+
+    private Long user_id;
+    private String name;
+    private String nickname;
+    private String id;
+    private String password;
+    private String email;
+    private Date birth;
+    private Role role;
+    private char user_stateus;
+
+    public loginDTO() {
+
+    }
+
+    public enum Role {
+        일반, 관리자
+    }
+
+    public loginDTO(Long user_id, String name, String nickname, String id, String password, String email, Date birth, Role role, char user_stateus) {
+        this.user_id = user_id;
+        this.name = name;
+        this.nickname = nickname;
+        this.id = id;
+        this.password = password;
+        this.email = email;
+        this.birth = birth;
+        this.role = role;
+        this.user_stateus = user_stateus;
+    }
+
+    public Long getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Long user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public char getUser_stateus() {
+        return user_stateus;
+    }
+
+    public void setUser_stateus(char user_stateus) {
+        this.user_stateus = user_stateus;
+    }
+}

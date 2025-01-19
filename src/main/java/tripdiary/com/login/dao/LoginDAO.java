3784dfab5d35b47db11dd66ee39c3ffbb6ae40ed
jeon <jejjb06@gmail.com>
2025-01-20 01:55:43 +0900

    회원가입 도전기

--- src/main/java/tripdiary/com/login/dao/LoginDAO.java
+++ src/main/java/tripdiary/com/login/dao/LoginDAO.java
@@ -1,12 +1,16 @@
 package tripdiary.com.login.dao;
 
+import org.egovframe.rte.psl.dataaccess.EgovAbstractMapper;
 import org.egovframe.rte.psl.dataaccess.mapper.Mapper;
 import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Repository;
 import tripdiary.com.login.vo.LoginVO;
 
-@Mapper("loginDAO")
-public class LoginDAO {
-    public void insertLogin(LoginVO loginVO) throws DataAccessException{
+@Mapper
+@Repository("loginDAO")
+public class LoginDAO extends EgovAbstractMapper {
 
-    };
+    public int insertMember(LoginVO vo) {
+        return insert("loginDAO.insertMember", vo);
+    }
 }
